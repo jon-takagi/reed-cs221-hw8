0f6af49240db0421aa3c2f4b72b20aac34161f0e
vagrant <vagrant@ubuntu>
2018-11-18 07:35:58 +0000

    Wrote a line or two.

--- cities_testing.cc
+++ cities_testing.cc
@@ -8,7 +8,19 @@
 #include "catch.hpp"
 #include "cities.hh"
 
-TEST_CASE("Reordering: reorder function")
+TEST_CASE("Random Permutations: random_permutation method should 
+
+/*
+TEST_CASE("Reordering: reorder method for Cities class. Given a permutation and returns a Cities object.")
 {
+    //First we create a city object, and fill it with 
+    //First we establish a couple vectors to test for reordering.
+    //These will be the following:
+    //test_perm_0 : {5, 4, 3, 2, 1, 0} B
+    //test_perm_1 :
+    //test_perm_2 :
+    permutation
+    SECTION
     REQUIRE(1 == 1);
 }
+*/
