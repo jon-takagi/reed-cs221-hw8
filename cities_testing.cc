de154c087a0ce4281a417146436b3f995ab03f6e
vagrant <vagrant@ubuntu>
2018-11-18 21:37:59 +0000

    Started testing.

--- cities_testing.cc
+++ cities_testing.cc
@@ -8,7 +8,12 @@
 #include "catch.hpp"
 #include "cities.hh"
 
-TEST_CASE("Random Permutations: random_permutation method should 
+TEST_CASE("Random Permutations: random_permutation method should create a randomized vector of unsigned integers.")
+{
+    //Unfortunately we made the mistake of making 
+    SECTION("Random permutation should contain all elements from zero to len - 1.")
+    {
+        
 
 /*
 TEST_CASE("Reordering: reorder method for Cities class. Given a permutation and returns a Cities object.")
@@ -24,3 +29,9 @@ TEST_CASE("Reordering: reorder method for Cities class. Given a permutation and 
     REQUIRE(1 == 1);
 }
 */
+
+//Outline:
+//Random perm: Test it has all the elements, and test each rand perm is different. Repeat for two lengths.\
+//Total Path distance: Test that it has the correct value, test for several city paths (but easy stuff, squares etc.)
+// >> << are tested visually/in programs (explain that in comment)
+//reorder: test that reordering works with one cities object and the outline above; use string stream (Sstream << cities, then Sstream >> string that's tested)
