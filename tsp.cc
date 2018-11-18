dfb4d718236cf6c21055ee7cec4789c16292f695
vagrant <vagrant@ubuntu>
2018-11-18 04:34:39 +0000

    think this is ready to test

--- tsp.cc
+++ tsp.cc
@@ -2,6 +2,8 @@
 #include <fstream>
 #include "cities.cc"
 int main(int argc, char** argv) {
+    int iterations = 1000000;
+
     if(argc != 2) {
         std::cerr << "Incorrect number of arguments" << "\n";
         exit(11);
@@ -11,6 +13,20 @@ int main(int argc, char** argv) {
     Cities city({});
     fs >> city;
     fs.close();
-    std::cout << city;
+
+    Cities best_city({});
+    double best_distance = std::numeric_limits<double>::infinity();
+    for(int i = 0; i < iterations; i++) {
+        city.reorder();
+        double dist = city.total_path_distance();
+        if(dist < best_distance) {
+            best_distance = dist;
+            best_city = city;
+            std::cout << i << "\t" << best_distance;
+        }
+    }
+
+    std::ofstream outstream("shortest.tsv");
+    outstream << best_city;
     return 0;
 }
